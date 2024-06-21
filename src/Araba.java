a7c8ae44420f4b720c9d95c2f27952fe47f9c477
kaanbalta <kaanbalta41@gmail.com>
2024-06-21 21:12:53 +0300

    oop1

---
+++ src/Araba.java
@@ -0,0 +1,54 @@
+public class Araba {
+
+    private String renk;
+    private int kapilar;
+    private int tekerlekler;
+    private String motor;
+    private String model;
+
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public String getRenk() {
+        return renk;
+    }
+
+    public void setRenk(String renk) {
+        this.renk = renk;
+    }
+
+    public int getKapilar() {
+        return kapilar;
+    }
+
+    public void setKapilar(int kapilar) {
+        if(kapilar<0){
+            System.out.println("Kapı sayısı 0dan küçük olamaz");
+        }
+        else {
+            this.kapilar = kapilar;
+        }
+    }
+
+    public int getTekerlekler() {
+        return tekerlekler;
+    }
+
+    public void setTekerlekler(int tekerlekler) {
+        this.tekerlekler = tekerlekler;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String [] args){
+
+        Araba mercedes = new Araba();
+        mercedes.setModel("cla 200");
+        System.out.println(mercedes.getModel());
+        mercedes.setKapilar(-2);
+
+
+    }
+}
